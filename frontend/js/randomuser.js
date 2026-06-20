7a994c5f8dfe3091ba9a3fa4364b98b9f911ab92
sainandhinigundu <sainandhinigundu28@gmail.com>
2026-06-20 10:33:45 +0530

    Added Random User Card

---
+++ frontend/js/randomuser.js
@@ -0,0 +1,21 @@
+function randomuser() {
+    fetch("https://randomuser.me/api/")
+       .then(function(res){
+           return res.json();
+       })
+       .then(function(data){
+          var user = data.results[0];
+          var fullName = user.name.title + " " + user.name.first + " " + user.name.last;
+          //image
+          document.getElementById("user-img").src=user.picture.large;
+
+          //name
+          document.getElementById("user-name").innerText=fullName;
+
+          //gender
+          document.getElementById("user-gender").innerText=user.gender; 
+       })
+       .catch(function(err) {
+           console.log(Error +err);
+       })
+}
